78b91c65bc17d4502956defd4ce478301f8ff3fd
Philip Jacobs <pdjny1972@hotmail.com>
2013-07-10 22:14:55 -0400

    first commit

---
+++ Jun27/view.h
@@ -0,0 +1,26 @@
+//
+//  view.h
+//  Jun27
+//
+//  Created by PHILIP JACOBS on 7/10/13.
+//  Copyright (c) 2013 PHILIP JACOBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView {
+	CGFloat var1;
+	CGFloat var2;
+	CGFloat var3;
+	BOOL ascent1;
+	BOOL ascent2;
+	BOOL ascent3;
+	CGFloat factor1;
+	CGFloat factor2;
+	CGFloat factor3;
+	int sizeVar;
+	int sizeIncr;
+	int rotateVar;
+	}
+
+@end
